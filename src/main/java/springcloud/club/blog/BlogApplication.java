ae01249e29f95846081c3762590104d598a5bc46
zhangjing <503951608@qq.com>
2020-01-07 11:38:51 +0800

    显示加载bean

--- src/main/java/springcloud/club/blog/BlogApplication.java
+++ src/main/java/springcloud/club/blog/BlogApplication.java
@@ -1,13 +1,24 @@
 package springcloud.club.blog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Slf4j
 public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(BlogApplication.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        log.info("bean总数:{}", ctx.getBeanDefinitionCount());
+        int i = 0;
+        for (String str : beanNames) {
+            log.info("{},beanName:{}", ++i, str);
+        }
     }
 
 }
